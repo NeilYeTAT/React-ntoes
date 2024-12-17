b1341480b5bc3fc58179f8d3b62b30d8a81fb098
Ye Yu <nearjilt@gmail.com>
2024-12-17 11:00:12 +0800

    todo: 调试

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useLayoutEffect, useState } from "react";
+
+async function queryData() {
+  const data = await new Promise<number>((resolve) => {
+    setTimeout(() => {
+      resolve(666);
+    }, 2000);
+  });
+  return data;
+}
+
 function App() {
-  return <div>app</div>;
+  const [num, setNum] = useState(0);
+
+  useLayoutEffect(() => {
+    queryData().then((data) => {
+      setNum(data);
+    });
+  }, []);
+
+  return (
+    <div
+      onClick={(e) => {
+        setNum((prevNum) => prevNum + 1);
+      }}
+    >
+      {num}
+    </div>
+  );
 }
 
 export default App;
